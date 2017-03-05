e9717cdeed0bfc43fc10fdfc30bad5078406f545
Miar412 <miarcompanies@gmail.com>
2017-03-05 14:16:24 +0100

    RUNME and Makefile updated

--- sdn-wise/RUNME.sh
+++ sdn-wise/RUNME.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-TARGET="sky"
+TARGET="z1"
 
-mkdir cooja_firmwares
+#mkdir cooja_firmwares
 
-make TARGET=$TARGET DEFINES=COOJA=1,SINK=1 
-mv sdn-wise.$TARGET cooja_firmwares/sink.$TARGET 
-make TARGET=$TARGET DEFINES=COOJA=1,SINK=0
-mv sdn-wise.$TARGET cooja_firmwares/node.$TARGET 
-make TARGET=$TARGET clean
+make TARGET=$TARGET DEFINES=COOJA=0,SINK=1 
+#mv sdn-wise.$TARGET cooja_firmwares/sink.$TARGET 
+mv sdn-wise.$TARGET sdn-wise-sink.$TARGET
+make TARGET=$TARGET DEFINES=COOJA=0,SINK=0
+#mv sdn-wise.$TARGET cooja_firmwares/node.$TARGET 
+mv sdn-wise.$TARGET sdn-wise-node.$TARGET
+#make TARGET=$TARGET clean
