8bfadd99392ce84efcd6071b477be477a91bdff2
Million Aregawi Beyene <miarcompanies@gmail.com>
2017-03-14 21:21:58 +0100

    RUNME small modification

--- sdn-wise/RUNME.sh
+++ sdn-wise/RUNME.sh
@@ -3,10 +3,12 @@ TARGET="z1"
 
 #mkdir cooja_firmwares
 
-make TARGET=$TARGET DEFINES=COOJA=0,SINK=1 
+#make TARGET=$TARGET DEFINES=COOJA=0,SINK=1 
+make TARGET=$TARGET DEFINE=SINK=1
 #mv sdn-wise.$TARGET cooja_firmwares/sink.$TARGET 
 mv sdn-wise.$TARGET sdn-wise-sink.$TARGET
-make TARGET=$TARGET DEFINES=COOJA=0,SINK=0
+#make TARGET=$TARGET DEFINES=COOJA=0,SINK=0
+make TARGET=$TARGET DEFINE=SINK=0
 #mv sdn-wise.$TARGET cooja_firmwares/node.$TARGET 
 mv sdn-wise.$TARGET sdn-wise-node.$TARGET
 #make TARGET=$TARGET clean
