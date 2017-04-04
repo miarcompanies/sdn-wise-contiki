ae072835e1794aa4f365744ea873c3446107b24c
miarcompanies <miarcompanies@gmail.com>
2017-04-04 02:53:55 +0200

    reset and update topo time modified to 25 and 80 seconds

--- sdn-wise/sdn-wise.c
+++ sdn-wise/sdn-wise.c
@@ -420,7 +420,7 @@
     while(1) {
       //Million slow the timer from 3 to 15
       //etimer_set(&et, 3 * CLOCK_SECOND);
-      etimer_set(&et, 45 * CLOCK_SECOND);
+      etimer_set(&et, 25 * CLOCK_SECOND);
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
       //Million reset timer to display neighbor table every 15 seconds
       etimer_reset(&et);
@@ -436,7 +436,7 @@
     while(1) {
       //Million slow the timer from 3 to 15
       //etimer_set(&et, 3 * CLOCK_SECOND);
-      etimer_set(&et, 180 * CLOCK_SECOND);
+      etimer_set(&et, 80 * CLOCK_SECOND);
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
       //Million reset timer to display neighbor table every 15 seconds
       etimer_reset(&et);
