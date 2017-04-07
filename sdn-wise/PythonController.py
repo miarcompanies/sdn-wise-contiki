f5a58324fd07b8e7b36315d317b36cc109c89e5b
Miar412 <miarcompanies@gmail.com>
2017-04-07 23:21:28 +0200

    PythonController Added

---
+++ sdn-wise/PythonController.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import time
+import serial
+import networkx as nx
+print("Simple Python Controller for SDN-WISE Starting .....")
+ser = serial.Serial('/dev/ttyUSB0')
+time.sleep(50)
+status = raw_input('Please enter your command - write Exit to quit\n')
+while 1:
+	ba = bytearray(status)
+	ser.write(ba)
+	if status == 'Exit':
+		ser.close()
+		break
+	status = raw_input('Please enter your command - write Exit to quit\n')
